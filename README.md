62007606cab947f54761c133b39cb47955adc098
Max Meisinger <mmeising@student.42heilbronn.de>
2021-12-07 19:29:51 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# get_next_line
+# get_next_line - 42 Heilbronn
+Function to read and save one line at a time out of a given file descriptor.
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/28 20:59:36 by mmeising          #+#    #+#             */
+/*   Updated: 2021/09/03 21:13:25 by mmeising         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_memset(void *str, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = str;
+	while (i < len)
+	{
+		*ptr = c;
+		i++;
+		ptr++;
+	}
+	return (str);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, '\0', n);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
+
+/*
+ *	frees buf[fd] and ret in case read fails (error or no new content)
+ */
+void	*ft_read_fail(char **buf, int fd, char *ret)
+{
+	free(buf[fd]);
+	buf[fd] = NULL;
+	free(ret);
+	ret = NULL;
+	return (NULL);
+}
